c21b926815bc67627b75e4d0c1d64af01819a9dc
akashprajapati9548 <akp954834@gmail.com>
2025-06-16 15:53:48 +0530

    Update start.py

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -6,31 +6,28 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
-        ],
-
+            InlineKeyboardButton(text=["S_B_2"], url=config.SUPPORT_GROUP),
+        ]
+    ]
     return buttons
 
 def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text=["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true"
             )
         ],
         [
-            InlineKeyboardButton(text=_["E_X_1"], url="https://github.com/NoxxOP/ShrutiMusic/fork")
+            InlineKeyboardButton(text=["E_X_1"], url="https://github.com/NoxxOP/ShrutiMusic"),
+            InlineKeyboardButton(text=["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
-        ],
+            InlineKeyboardButton(text=["S_B_4"], callback_data="settings_back_helper"),
+        ]
     ]
     return buttons
